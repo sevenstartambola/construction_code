0d417ed3857fb4e26db8b6a33acde0ea528a1900
Abinash Sonar <74831561+abinash0013@users.noreply.github.com>
2024-08-19 17:23:19 +0530

    changes on main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,14 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center p-0">
-      <video
-        className="w-full object-cover"
-        controls
-        autoPlay
-        loop
-        muted
-        preload="none" 
-        style={{ height: '600px' }} // Set your custom height here
-      >
-        <source src="/vT8KUZ1aLJS6H5Kr2F.mp4" type="video/mp4" />
-      </video>
+    <main className="flex items-center justify-center"> 
+        <Image
+          src="/under.jpg" 
+          alt="Example Image"
+          height={0}
+          width={950}
+        /> 
     </main>
   );
 }
